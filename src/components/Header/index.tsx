d7133521c2e809403c549fbeda0a76fa83a90c55
Alan Almeida <alanalmeida.emp@gmail.com>
2021-03-28 18:32:42 -0300

    created ActiveLink component and finshed prismic setup

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,24 @@
 import { SignInButton } from '../SiginInButton'
+import Link from 'next/link'
+
 import styles from './styles.module.scss'
+import { ActiveLink } from '../ActiveLink'
 
 export function Header() {
+
 	return (
 		<header className={styles.headerContainer}>
 			<div className={styles.headerContent}>
-				<img src="/images/logo.svg" alt="ig.news"/>
+				<Link href="/">
+					<img src="/images/logo.svg" alt="ig.news"/>
+				</Link>
 				<nav>
-					<a className={styles.active}>Home</a>
-					<a>Posts</a>
+					<ActiveLink activeClassName={styles.active} href="/">
+						<a>Home</a>
+					</ActiveLink>
+					<ActiveLink activeClassName={styles.active} href="/posts" prefetch>
+						<a>Posts</a>				
+					</ActiveLink>
 				</nav>
 
 				<SignInButton />
